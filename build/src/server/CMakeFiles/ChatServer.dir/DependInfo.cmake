750dfd570f82da2b4a1d705744b7d07c36c6dc57
100ask <user@100ask.com>
2024-05-30 05:56:35 -0400

    add auto compile shell

---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/book/Chat/ChatServer/src/server/chatserver.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/book/Chat/ChatServer/src/server/chatservice.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/book/Chat/ChatServer/src/server/db/db.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/db/db.cpp.o"
+  "/home/book/Chat/ChatServer/src/server/main.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/book/Chat/ChatServer/src/server/model/friendmodel.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/book/Chat/ChatServer/src/server/model/groupmodel.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/book/Chat/ChatServer/src/server/model/offlinemessagemodel.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/book/Chat/ChatServer/src/server/model/usermodel.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/book/Chat/ChatServer/src/server/redis/redis.cpp" "/home/book/Chat/ChatServer/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../thirdparty"
+  "../include/server/db"
+  "../include/server/model"
+  "../include/server/redis"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
